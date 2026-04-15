e88af8b4e3d868b58863680c07e45cae471ed607
FelicitasGarcia <felicitasgarcia01@gmail.com>
2026-04-15 14:56:04 -0300

    Afuera el file del wrapper

--- mm_fuzzer/pua_wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Resetear el veredicto ANTES de cada ejecucion para evitar valores stale
-echo -n "0" > /tmp/mm_verdict
-
-exec /Users/felicitasgarcia/MM/mimicrymonitor/llvm/feli/outputs/instrumentedPUA "$1"
